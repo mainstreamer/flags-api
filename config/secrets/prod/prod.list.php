a3874e765e3d9593395a86af5e9db5029581207e
Artem Dolmatov <artem@f6s.com>
2021-01-13 00:43:27 +0200

    secrets added

---
+++ config/secrets/prod/prod.list.php
@@ -0,0 +1,10 @@
+<?php
+
+return array (
+  'BOT_TOKEN' => NULL,
+  'CORS_ALLOW_ORIGIN' => NULL,
+  'JWT_PASSPHRASE' => NULL,
+  'JWT_PUBLIC_KEY' => NULL,
+  'JWT_SECRET_KEY' => NULL,
+  'JWT_TOKEN_TTL' => NULL,
+);
