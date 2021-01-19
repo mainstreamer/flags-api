2379e416e56029006265306bb622f7c12fee1fd1
Artem Dolmatov <artem@f6s.com>
2021-01-20 01:04:32 +0200

    docker config and symfony upgrade

--- config/bundles.php
+++ config/bundles.php
@@ -7,6 +7,5 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
-    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
