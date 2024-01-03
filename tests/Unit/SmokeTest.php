e00edfc188dd96f6ee4cb1351636eddedd438526
Artem Dolmatov <rteeom@outlook.com>
2024-01-03 20:38:28 +0000

    fixed test

--- tests/Unit/SmokeTest.php
+++ tests/Unit/SmokeTest.php
@@ -50,7 +50,7 @@ class SmokeTest extends KernelTestCase
                     'capitals-africa.json' => self::assertCount(54, $countries),
                     'capitals-americas.json' => self::assertCount(55, $countries),
                     'capitals-asia.json' => self::assertCount(48, $countries),
-                    'capitals-europe.json' => self::assertCount(46, $countries),
+                    'capitals-europe.json' => self::assertCount(45, $countries),
                     'capitals-oceania.json' => self::assertCount(25, $countries),
                 };
             }
