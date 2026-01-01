a9b7fce9981bd1c72c5862c35e1e5df51a0df3bb
m::r <mainstreamer@outlook.com>
2026-01-01 04:11:45 +0000

    fix(jwks): attempt 3

--- app/src/Flags/ConsoleCommand/WarmupJwksCommand.php
+++ app/src/Flags/ConsoleCommand/WarmupJwksCommand.php
@@ -5,17 +5,21 @@ declare(strict_types=1);
 namespace App\Flags\ConsoleCommand;
 
 use App\Flags\Service\JwksService;
+use Override;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
+/**
+ * @psalm-suppress UnusedClass
+ */
 #[AsCommand(
     name: 'app:jwks:warmup',
     description: 'Fetch and cache the public key from JWKS endpoint',
 )]
-class WarmupJwksCommand extends Command
+final class WarmupJwksCommand extends Command
 {
     public function __construct(
         private readonly JwksService $jwksService,
@@ -23,6 +27,7 @@ class WarmupJwksCommand extends Command
         parent::__construct();
     }
 
+    #[Override]
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
