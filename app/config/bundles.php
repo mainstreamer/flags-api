a80d2bed735023d36d318a48dc1800492c0b5307
m::r <mainstreamer@outlook.com>
2026-01-01 01:08:16 +0000

    feat(sentry): initial sentry config and pipeline change

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -8,4 +8,5 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     KnpU\OAuth2ClientBundle\KnpUOAuth2ClientBundle::class => ['all' => true],
+    Sentry\SentryBundle\SentryBundle::class => ['all' => true],
 ];
