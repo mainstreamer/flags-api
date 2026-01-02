c1017ebb0f8d241742c314086b2fd1de4f0ab625
m::r <mainstreamer@outlook.com>
2026-01-02 21:56:48 +0000

    fix(pipeline): disable phpcs pipeline

--- app/src/Flags/Entity/Answer.php
+++ app/src/Flags/Entity/Answer.php
@@ -41,7 +41,7 @@ class Answer
     #[ORM\Column(type: 'string', length: 255)]
     protected private(set) string $answerOptions {
         get => $this->answerOptions;
-        set(array|string $value) {
+        set(array | string $value) {
             $this->answerOptions = is_array($value) ? json_encode($value, JSON_THROW_ON_ERROR) : $value;
         }
     }
@@ -84,7 +84,8 @@ class Answer
         $item->timer = $array['time'] ?? 0;
         $item->correct = $array['correct'] ?? true;
         $item->date = isset($array['answerDateTime']) ? new \DateTime()->setTimestamp(
-            round($array['answerDateTime'] / 1000))
+            round($array['answerDateTime'] / 1000)
+        )
         : new \DateTime();
 
         return $item;
