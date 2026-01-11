092328b24eccaa0125e127ff04ffed93bdf2540c
m::r <mainstreamer@outlook.com>
2026-01-11 20:05:11 +0000

    codestyle fix

--- app/tests/Unit/Service/CountriesGeneratorTest.php
+++ app/tests/Unit/Service/CountriesGeneratorTest.php
@@ -4,12 +4,8 @@ declare(strict_types=1);
 
 namespace App\Tests\Unit\Service;
 
-use App\Flags\Repository\UserRepository;
-use App\Flags\Security\HqAuthAuthenticator;
-use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Intl\Countries;
-use Symfony\Component\Routing\RouterInterface;
 
 class CountriesGeneratorTest extends TestCase
 {
